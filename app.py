9b1da4edf3dafd25f6ae548f67119f1dcc7e6521
EverDCA <Evdacar18@gmail.com>
2025-02-13 13:20:40 -0500

    update

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return redirect('/login')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/recover')
+def recover():
+    return render_template('recover.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
